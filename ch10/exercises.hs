51b1b976f89a1176cb706cc0177d0db32642ab0b
Rokas Gegevičius <rokas@rgl.lt>
2023-06-12 18:01:43 +0300

    more folds

--- ch10/exercises.hs
+++ ch10/exercises.hs
@@ -1,9 +1,47 @@
-f1 = foldr (++) "" ["woot", "WOOT", "woot"]
-f2 = foldr max [] ["fear is the little death"]
-f3 = foldr (&&) True [False, True]
-f4 = foldr (||) True [False, True]
-f5 = foldl (flip $ (++) . show) "" [1..5]
-f6 = foldr (flip const) 'a' [1..5]
-f7 = foldr const 'a' "tacos"
-f8 = foldl const 0 "burritos"
-f9 = foldl const 'z' [1..5]
+------------------------------------------------------------------
+--Chapter Exercises
+------------------------------------------------------------------
+
+-- Rewriting functions using folds
+-- 1.
+myOr :: [Bool] -> Bool
+myOr = foldr (||) False
+-- 2.
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f = myOr . map f
+-- 3.
+myElemR :: Eq a => a -> [a] -> Bool
+myElemR a [] = False
+myElemR a (x:xs)
+  | x == a = True
+  | otherwise = myElemR a xs
+
+myElemF :: Eq a => a -> [a] -> Bool
+myElemF n = foldr (\a b -> b || a == n) False
+
+myElem :: Eq a => a -> [a] -> Bool
+myElem n = any (==n)
+-- 4.
+myReverse :: [a] -> [a]
+myReverse = foldl (flip (:)) []
+-- 5.
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldr (\a b -> f a : b) []
+-- 6.
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f = foldr (\a b -> if f a == True then a : b else b) []
+-- 7.
+squish :: [[a]] -> [a]
+squish = foldr (++) []
+-- 8.
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f = foldr (\a b -> f a ++ b) []
+-- 9.
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+-- 10.
+myMaximumBy :: Num a => (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f = foldl (\a b -> if (f a b) == GT then a else b) 1
+-- 11.
+myMinimumBy :: Num a => (a -> a -> Ordering) -> [a] -> a
+myMinimumBy f = foldl (\a b -> if (f a b) == LT then a else b) 1
