7191a13ba8d13bb205e609fd69c30d6ac140e7a7
Rokas Gegevičius <rokas@rgl.lt>
2023-06-13 10:40:32 +0300

    foldr && foldl

--- ch10/exercises.hs
+++ ch10/exercises.hs
@@ -1,4 +1,57 @@
 ------------------------------------------------------------------
+--Intermission: Exercises
+------------------------------------------------------------------
+{-
+1. b, c
+2.
+foldl :: (b -> a -> b) -> b -> [a] -> b
+foldl f acc xs =
+  case xs of
+    []     -> acc
+    (x:xs) -> foldl f (foldl f acc x) xs
+
+foldl (flip (*)) [1..3]
+
+foldl (foldl (flip (*)) ((flip (*)) 1 1) [2,3]
+foldl (foldl (flip (*)) ((flip (*)) ((flip (*)) 1 1) 2) [3]
+foldl (foldl (flip (*)) ((flip (*)) ((flip (*)) ((flip (*)) 1 1) 2) 3) []
+((flip (*)) ((flip (*)) ((flip (*)) 1 1) 2) 3)
+((flip (*)) ((flip (*)) 1 2) 3)
+((flip (*)) 2 3)
+= 6
+------------------------------------------------------------------
+3. c
+4. a
+-}
+--5.
+--foldr (a -> b -> b) -> b -> [a] -> b
+--foldl (b -> a -> b) -> b -> [a] -> b
+f1 = foldr (++) "" ["woot", "WOOT", "woot"]
+f2 = foldr max ' ' "fear is the little death"
+f3 = foldr (&&) True [False, True]
+f4 = foldr (||) False [False, True]
+f5 = foldr ((++) . show) "" [1..5]
+f6 = foldl const 'a' [1..5]
+f7 = foldr (flip const) 0 "tacos"
+ft = foldl const 0 "tacos"
+f8 = foldr (flip const) 0 "burritos"
+f9 = foldr (flip const) 'z' [1..5]
+------------------------------------------------------------------
+--Scans Exercises
+------------------------------------------------------------------
+fibs = 1 : scanl (+) 1 fibs
+fibsN x = fibs !! x
+--1.
+fibs20 = take 20 fibs
+--2.
+fibs100 = takeWhile (<100) fibs
+--3.
+fact :: [Integer]
+fact = scanl (*) 1 [1 .. ]
+
+factN :: Integer -> Integer
+factN x = fact !! fromIntegral x
+------------------------------------------------------------------
 --Warm-up and review
 ------------------------------------------------------------------
 --1.
@@ -8,15 +61,11 @@ seekritFunc :: String -> Int
 seekritFunc x =
   div (sum (map length (words x)))
       (length (words x))
-
+--3.
 seekritFuncPrec :: String -> Double
 seekritFuncPrec x =
   (/) (fromIntegral (sum (map length (words x))))
       (fromIntegral (length (words x)))
-
-
-
-
 ------------------------------------------------------------------
 --Chapter Exercises
 ------------------------------------------------------------------
